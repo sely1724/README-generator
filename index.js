af77be5545a66bf2565b5b6829cf8638f211f458
Sylvia Ely <sylvianne9417@gmail.com>
2022-11-24 13:55:19 -0800

    added prompts

--- index.js
+++ index.js
@@ -1,13 +1,82 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the title of your project?",
+  "Describe your project",
+  "Add Installation Instructions",
+  "Add Usage Instructions",
+  "Choose a License",
+  "Add Contribution Instructions",
+  "Add Testing Instructions",
+  "What is your Github Username?", //might not need in array?
+  "What is your email address?",
+  //"Table of Contents", //Not Needed - Internally Write
+];
+
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init();
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: questions[0],
+    name: "questionsTitle",
+  },
+  {
+    type: "input",
+    message: questions[1],
+    name: "questionsDescr",
+  },
+  {
+    type: "input",
+    message: questions[2],
+    name: "questionsInstall",
+  },
+  {
+    type: "input",
+    message: questions[3],
+    name: "questionsDescr",
+  },
+  {
+    //license
+    type: "list",
+    message: questions[4],
+    choices: [
+      "MIT License",
+      "ISC License",
+      "Apache License 2.0",
+      "GNU General Public License v3.0",
+    ],
+    name: "questionsList",
+  },
+  {
+    type: "input",
+    message: questions[5],
+    name: "questionsContribution",
+  },
+  {
+    type: "input",
+    message: questions[6],
+    name: "questionsTesting",
+  },
+  {
+    type: "input",
+    message: questions[7],
+    name: "questionsGithubUser",
+  },
+  {
+    type: "input",
+    message: questions[8],
+    name: "questionsEmailAddress",
+  },
+]);
