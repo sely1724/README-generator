175d24667cf545864a977cdf07985b056a834490
Sylvia Ely <sylvianne9417@gmail.com>
2022-11-26 11:26:42 -0800

    console log testing.  Now to figure out how to integrate markdown template

--- index.js
+++ index.js
@@ -16,67 +16,91 @@ const questions = [
 
 const fs = require("fs");
 const inquirer = require("inquirer");
+const markdownTemplate = require("./utils/generateMarkdown");
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-//function init() {}
+//function init() {}f
 
 // Function call to initialize app
 //init();
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: questions[0],
-    name: "questionsTitle",
-  },
-  {
-    type: "input",
-    message: questions[1],
-    name: "questionsDescr",
-  },
-  {
-    type: "input",
-    message: questions[2],
-    name: "questionsInstall",
-  },
-  {
-    type: "input",
-    message: questions[3],
-    name: "questionsDescr",
-  },
-  {
-    //license
-    type: "list",
-    message: questions[4],
-    choices: [
-      "MIT License",
-      "ISC License",
-      "Apache License 2.0",
-      "GNU General Public License v3.0",
-    ],
-    name: "questionsList",
-  },
-  {
-    type: "input",
-    message: questions[5],
-    name: "questionsContribution",
-  },
-  {
-    type: "input",
-    message: questions[6],
-    name: "questionsTesting",
-  },
-  {
-    type: "input",
-    message: questions[7],
-    name: "questionsGithubUser",
-  },
-  {
-    type: "input",
-    message: questions[8],
-    name: "questionsEmailAddress",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: questions[0],
+      name: "questionsTitle",
+    },
+    {
+      type: "input",
+      message: questions[1],
+      name: "questionsDescr",
+    },
+    {
+      type: "input",
+      message: questions[2],
+      name: "questionsInstall",
+    },
+    {
+      type: "input",
+      message: questions[3],
+      name: "questionsUsage",
+    },
+    {
+      //license
+      type: "list",
+      message: questions[4],
+      choices: [
+        "MIT License",
+        "ISC License",
+        "Apache License 2.0",
+        "GNU General Public License v3.0",
+      ],
+      name: "questionsLicense",
+    },
+    {
+      type: "input",
+      message: questions[5],
+      name: "questionsContribution",
+    },
+    {
+      type: "input",
+      message: questions[6],
+      name: "questionsTesting",
+    },
+    {
+      type: "input",
+      message: questions[7],
+      name: "questionsGithubUser",
+    },
+    {
+      type: "input",
+      message: questions[8],
+      name: "questionsEmailAddress",
+    },
+  ])
+  .then((response) => {
+    const title = response.questionsTitle;
+    const descr = response.questionsDescr;
+    const install = response.questionsInstall;
+    const usage = response.questionsUsage;
+    const license = response.questionsLicense;
+    const contrib = response.questionsContribution;
+    const testing = response.questionsTesting;
+    const userName = response.questionsGithubUser;
+    const email = response.questionsEmailAddress;
+    const tableofContents = [
+      "Description",
+      "Installation",
+      "Usage",
+      "License",
+      "Contribution",
+      "Testing",
+      "GitHub Username",
+      "Email",
+    ];
+    console.log("Hello " + contrib);
+  });
