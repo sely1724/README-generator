492ecee9ba83f76007014c583f109fe3b16f9c82
Sylvia Ely <sylvianne9417@gmail.com>
2022-11-27 13:52:52 -0800

    figured out license section.  Deleted test markdown files.  Want to figure out how to REQUIRE a response in node. And then should be good

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license == "MIT License") {
     return `![${license}](https://img.shields.io/badge/license-MIT-green)`;
@@ -14,14 +12,28 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-//function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == "MIT License") {
+    return `https://opensource.org/licenses/MIT`;
+  } else if (license === "ISC License") {
+    return `https://opensource.org/licenses/ISC`;
+  } else if (license == "Apache License 2.0") {
+    return `https://opensource.org/licenses/Apache-2.0`;
+  } else if (license == "GNU General Public License v3.0") {
+    return `https://opensource.org/licenses/GPL-3.0`;
+  } else {
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  console.log(license);
+  if (license) {
+    return `License used for this project: [${license}](${renderLicenseLink(
+      license
+    )})\n Please refer to the license section of my REPO if additional information is needed`;
+  } else return ``;
 }
 
 // TODO: Create a function to generate markdown for README
@@ -59,10 +71,10 @@ ${response.questionsTesting}
 
 ## Questions
 Questions? Feel free to get in touch:
-GitHub: https://github.com/${response.questionsGitHubUser}
-Email: ${response.questionsEmailAddress}
+\n GitHub: https://github.com/${response.questionsGitHubUser}
+\n Email: ${response.questionsEmailAddress}
 
-`; //add direct email link???
+`;
 }
 
 module.exports = generateMarkdown;
